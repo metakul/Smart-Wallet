0b1a63fb550f7b45c5846b33f0fa68d4afe345a3
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-11-08 11:38:08 +0530

    test without otp on mainnet

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -1,26 +1,32 @@
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({
-  username: 'api',
-  key: '3e5c466e567bb74342ec59cf4674b275-8c9e82ec-2ec8b9d2',
+const nodemailer = require('nodemailer');
+
+// Configure Nodemailer with Gmail SMTP
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'shubhamkunwar10feb@gmail.com',
+    pass: 'hedb bryb fbbp uusi', // Use your Gmail password or an App Password
+  },
 });
 
 // Define a function that sends an email with the provided OTP
 const sendEmailWithOTP = (toEmail, otp) => {
-  mg.messages
-    .create('sandboxb3587f19e5a442c797200213290c1cb2.mailgun.org', {
-      from: 'Mailgun Sandbox <postmaster@sandboxb3587f19e5a442c797200213290c1cb2.mailgun.org>',
-      to: [toEmail],
-      subject: 'Your OTP Code',
-      text: `Your OTP code is: ${otp}`,
-    })
-    .then((msg) => {
-      console.log('OTP email sent:', msg); // logs response data
-    })
-    .catch((err) => {
-      console.error('Error sending OTP email:', err); // logs any error
-    });
+  // Compose the email
+  const mailOptions = {
+    from: 'metakul@gmail.com',
+    to: toEmail,
+    subject: 'Your OTP Code',
+    text: `Your OTP code is: ${otp}`,
+  };
+
+  // Send the OTP email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending OTP email:', error);
+    } else {
+      console.log('OTP email sent:', info.response);
+    }
+  });
 };
 
 module.exports = sendEmailWithOTP;
